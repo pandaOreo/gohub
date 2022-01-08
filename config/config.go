166dc4a1c48a0f9163aaddaafa4c32a14c747409
樊京华 <819023279@qq.com>
2022-01-08 16:11:48 +0800

    配置方案的实现

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+/**
+ * @Author       : Jinghua Fan
+ * @Date         : 2022-01-08 15:38:34
+ * @LastEditors  : Jinghua Fan
+ * @LastEditTime : 2022-01-08 15:38:53
+ * @Description  : 佛祖保佑,永无BUG
+ */
+
+// Initialize 触发加载 config 包的所有的 init 函数
+func Initialize() {
+
+}
--- main.go
+++ main.go
@@ -1,29 +1,44 @@
+package main
+
 /**
  * @Author       : Jinghua Fan
  * @Date         : 2022-01-08 08:59:14
  * @LastEditors  : Jinghua Fan
- * @LastEditTime : 2022-01-08 14:52:25
+ * @LastEditTime : 2022-01-08 15:44:12
  * @Description  : 佛祖保佑,永无BUG
  */
 
-package main
-
 import (
+	"flag"
 	"fmt"
 
 	"github.com/ZimoBoy/gohub/bootstrap"
+	btsConfig "github.com/ZimoBoy/gohub/config"
+	"github.com/ZimoBoy/gohub/pkg/config"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
-	// new 一个 GIN Engine 实例
-	r := gin.New()
+
+	// 配置初始化, 依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件, 如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
 	// 初始化路由绑定
-	bootstrap.SetupRoute(r)
+	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := r.Run(":8000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理, 端口被占用了或者其他错误
 		fmt.Println(err.Error())
