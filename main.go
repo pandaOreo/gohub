a984ca24863c6a4efca46f438103a479e15e9161
樊京华 <819023279@qq.com>
2022-01-08 20:15:30 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := router.Run(":" + config.Get("app.port"))
+	err := router.Run("localhost:" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理, 端口被占用了或者其他错误
 		fmt.Println(err.Error())
