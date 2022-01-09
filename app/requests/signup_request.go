de075d3634a7f648fdaa4e80d43af63f76111e5b
樊京华 <819023279@qq.com>
2022-01-09 14:12:12 +0800

    验证邮箱是否注册

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -18,6 +18,10 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" valid:"email"`
+}
+
 func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
@@ -29,6 +33,34 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		"phone": []string{
 			"required:手机号为必填项,参数与名称为phone",
 			"digits:手机号长度必须为11位的数字",
+		},
+	}
+
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid",
+		Messages:      messages,
+	}
+
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
+
+func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"email": []string{"required", "min:4", "max:30", "email"},
+	}
+
+	// 自定义验证出错时的提示
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email 为必填项",
+			"min:Email 长度需大于4",
+			"max:Email 长度需小于30",
+			"email:Email 格式不正确,请 提供有效的邮箱地址",
 		},
 	}
 
