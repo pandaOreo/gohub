fcc42929fe03b024ea512838991edd843e955572
樊京华 <819023279@qq.com>
2022-01-12 10:31:13 +0800

    邮箱+验证码注册

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -50,6 +50,7 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	})
 }
 
+// SignupUsingPhone 使用手机号 + 验证码注册
 func (sc *SignupController) SignupUsingPhone(c *gin.Context) {
 	// 1. 验证表单
 	request := requests.SignupUsingPhoneRequest{}
@@ -61,6 +62,29 @@ func (sc *SignupController) SignupUsingPhone(c *gin.Context) {
 	_user := user.User{
 		Name:     request.Name,
 		Phone:    request.Phone,
+		Password: request.Password,
+	}
+	_user.Create()
+
+	if _user.ID > 0 {
+		response.CreatedJSON(c, gin.H{
+			"data": _user,
+		})
+	} else {
+		response.Abort500(c, "创建用户失败, 请稍后再试~")
+	}
+}
+func (sc *SignupController) SignupUsingEmail(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.SignupUsingEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.SignupUsingEmail); !ok {
+		return
+	}
+
+	// 2. 验证成功,创建数据
+	_user := user.User{
+		Name:     request.Name,
+		Email:    request.Email,
 		Password: request.Password,
 	}
 	_user.Create()
