f49dbaa4b4ceedea366323b57d983ae373398c8f
樊京华 <819023279@qq.com>
2022-01-15 16:15:27 +0800

    用户列表

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"github.com/ZimoBoy/gohub/app/models/user"
 	"github.com/ZimoBoy/gohub/pkg/auth"
 	"github.com/ZimoBoy/gohub/pkg/response"
 
@@ -14,4 +15,10 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 	userModel := auth.CurrentUser(c)
 	response.Data(c, userModel)
+}
+
+// Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }
